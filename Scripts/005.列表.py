dd56181ae662083405ce94f6a5fd2d958319d7c8
Easy1229 <2859412930@qq.com>
2023-03-03 17:34:37 +0800

    Create 005.列表.py

---
+++ Scripts/005.列表.py
@@ -0,0 +1,15 @@
+computer_list=[]
+computer_list.append("CPU")
+computer_list.append("主板")
+computer_list.append("散热器")
+computer_list.append("内存条")
+computer_list.append("硬盘")
+computer_list.append("显卡")
+computer_list.append("电源")
+computer_list.append("风扇")
+computer_list.append("机箱")
+
+print("列表的长度为"+str(len(computer_list))+"\n")
+
+for item in computer_list:
+    print(item)
